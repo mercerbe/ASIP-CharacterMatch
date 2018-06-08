4a701b44b452ac3e3395b3bddf274bcbebcf284b
Admin <mercerbe@gmail.com>
2018-06-08 11:42:09 -0400

    survey setup, user data api added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ejs = require('ejs');
 const characters = require('../data/characters.js');
+const users = require('../data/users.js');
 
 module.exports = function(app){
 
@@ -8,5 +9,45 @@ module.exports = function(app){
   app.get('/api/characters', function(req,res){
     res.json(characters);
   });
+
+  //get all users
+  app.get('/api/users', function(req, res){
+    res.json(users);
+  });
+
+  //new character entry
+  app.post('api/characters', function(req, res){
+
+    //user input with path
+    let newUser = req.body;
+
+    //capture responses
+    let responses = input.scores;
+
+    //match
+    let matchName = '';
+    let matchImage = '';
+    let initial = 1000;
+
+    //loop through characters
+    for (let i = 0; i < characters.length; i++) {
+
+      //difference for each question
+      let diff = 0;
+      for (var j = 0; j < responses.length; j++) {
+        diff += Math.abs(characters[i].scores[j] - responses[j]);
+      }
+      //smallest diff = match
+      if(diff < inital){
+        inital = diff;
+        matchName = characters[i].name;
+        matchImage = characters[i].image;
+      }
+
+    }
+    //add to users
+    users.push(newUser);
+    res.json({status: 'ok', matchName: matchName, matchImage: matchImage});
+  })
 
 };
--- server.js
+++ server.js
@@ -5,22 +5,22 @@ const ejs = require('ejs');
 
 //Express
 const app = express();
+const PORT = 3000;
 
 //ejs views
 app.set('view engine', 'ejs');
-app.set('public', path.join(__dirname, './app/public'));
-app.use(express.static('public'));
 
 //Express use body-parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 
 //routing
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+app.use(express.static(__dirname + '/public'));
 
 //listen
 app.listen(3000, function(){
-  console.log('Listening on Port 3000...');
+  console.log('Listening on Port ' + PORT);
 })
